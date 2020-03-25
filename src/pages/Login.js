d8d08edaedae95b56a0f6026906b4a618bc0a184
Symon <ramossy@oregonstate.edu>
2020-03-25 14:07:55 -0700

    implement intial react hook form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
 
+import { getData, postData } from "../services/api";
 import { Paper, Grow, Grid } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -20,12 +22,42 @@ const Login = () => {
     window.matchMedia("(max-width: 768px)").addListener(handler);
   }, []);
 
+  const { handleSubmit, register, watch, errors } = useForm();
+  
+  const onSubmit = data => {
+    console.log(data)
+  };
+
+  //fixme
+  console.log(watch('example'));
+
   const body = (
     <Grid container>
       <Grid item xs={12}>
         <h1>Login</h1>
       </Grid>
       <Grid item xs={12}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+        name="email"
+        ref={register({
+          required: 'Required',
+          pattern: {
+            value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+            message: "invalid email address"
+          }
+        })}
+      />
+      {errors.email && errors.email.message}
+      <input
+        name="password"
+        ref={register({
+          required: 'Required'
+        })}
+      />
+      {errors.password && errors.password.message}
+      <button type="submit">Submit</button>
+        </form>
         login
       </Grid>
     </Grid>
