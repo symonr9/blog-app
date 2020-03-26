c992a4932257c49ada3e2d2977d4c4d3b157c111
Symon <ramossy@oregonstate.edu>
2020-03-25 22:55:26 -0700

    implement more create page functionality

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useForm } from "react-hook-form";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import { getData, postData } from "../services/api";
 
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import MUIRichTextEditor from "mui-rte";
 import PublishRoundedIcon from "@material-ui/icons/PublishRounded";
 import {
   Button,
@@ -23,35 +25,55 @@ const useStyles = makeStyles({
     marginTop: "20em"
   },
   typeSelect: {
-    marginBottom: '1em'
+    marginBottom: "1em"
   },
   submitBtnDiv: {
-    marginTop: '2em',
-    marginBottom: '2em'
+    marginTop: "2em",
+    marginBottom: "2em"
   },
   formInput: {
-    marginTop: '0.5em'
+    marginTop: "0.5em"
   }
 });
 
+const defaultTheme = createMuiTheme();
+
+Object.assign(defaultTheme, {
+  overrides: {
+    MUIRichTextEditor: {
+      root: {
+        marginTop: "0.5em",
+        width: "100%",
+        border: "1px solid lightgray",
+        borderRadius: "5px"
+      },
+      editor: {
+        height: "15em",
+        maxHeight: "100vh",
+        overflow: "auto",
+        marginLeft: "1em"
+      }
+    }
+  }
+});
 
 const poemTypes = [
-{
-  value: "prose",
-  label: "Prose"
-},
-{
-  value: "iambic-pentameter",
-  label: "Iambic Pentameter"
-},
-{
-  value: "sonnet",
-  label: "Sonnet"
-},
-{
-  value: "custom",
-  label: "Custom"
-}
+  {
+    value: "prose",
+    label: "Prose"
+  },
+  {
+    value: "iambic-pentameter",
+    label: "Iambic Pentameter"
+  },
+  {
+    value: "sonnet",
+    label: "Sonnet"
+  },
+  {
+    value: "custom",
+    label: "Custom"
+  }
 ];
 
 function Create() {
@@ -89,6 +111,12 @@ function Create() {
     setPoemType(event.target.value);
   };
 
+  let rtePoemInput = useRef(null);
+
+  const handleRTEClick = event => {
+    rtePoemInput.current.focus();
+  };
+
   const onSubmit = data => {
     console.log(data);
 
@@ -104,78 +132,103 @@ function Create() {
     */
   };
 
-  console.log(watch("example"));
-
-
   const submitBtn = (
     <div className={classes.submitBtnDiv}>
-    <Button
-      variant="contained"
-      color="primary"
-      startIcon={<PublishRoundedIcon />}
-      type="submit"
-    >
-      Publish
-    </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        startIcon={<PublishRoundedIcon />}
+        type="submit"
+      >
+        Publish
+      </Button>
     </div>
   );
+
+  const richTextEditor = (id, name, numOfLines = 1) => {
+    return <div>sds</div>;
+  };
+
+  const basicTextField = (name, label, numOfLines = 1) => {
+    return (
+      <TextField
+        name={name}
+        label={label}
+        placeholder="Start writing..."
+        variant="outlined"
+        multiline={!(numOfLines == 1)}
+        rows={numOfLines}
+        fullWidth
+        className={classes.formInput}
+      ></TextField>
+    );
+  };
+
+  const selectTextField = (name, label, value, onChangeFun, options) => {
+    return (
+      <TextField
+        name={name}
+        label={label}
+        placeholder="Start writing..."
+        variant="outlined"
+        value={value}
+        select
+        onChange={onChangeFun}
+        fullWidth
+        className={classes.formInput}
+      >
+        {options.map(option => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+    );
+  };
 
   const types = [
     {
       value: "poetry",
       label: "compose a poem",
-      formInput: <div className={classes.formDiv}>
-                       <TextField
-            name="title"
-            label="Title of your masterpiece"
-            placeholder="Start writing..."
-            variant="outlined"
-            fullWidth
-            className={classes.formInput}
-          /> 
-               <TextField
-            name="body"
-            label="Body of your masterpiece"
-            multiline
-            rows="10"
-            placeholder="Start writing..."
-            variant="outlined"
-            fullWidth
-            className={classes.formInput}
+      formInput: (
+        <div className={classes.formDiv}>
+          {basicTextField("title", "Title of your masterpiece")}
+
+          <MuiThemeProvider theme={defaultTheme}>
+            <MUIRichTextEditor
+              id="rteId"
+              name="body"
+              inlineToolBar={true}
+              placeholder="Start writing..."
+              variant="outlined"
+              fullWidth
+              inputRef={rtePoemInput}
+              onClick={handleRTEClick}
+              className={classes.formInput}
             />
-          <TextField
-            name="type"
-            label="Type of your poem"
-            placeholder="Start writing..."
-            select
-            value={poemType}
-            onChange={handlePoemTypeChange}
-            variant="outlined"
-            fullWidth
-            className={classes.formInput}
-          >
-            {poemTypes.map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
-              </MenuItem>
-            ))}
-          </TextField>
-          <TextField
-            name="notes"
-            label="Notes of your poem"
-            multiline
-            rows="2"
-            placeholder="Start writing..."
-            variant="outlined"
-            fullWidth
-            className={classes.formInput}
-          /> 
-        {submitBtn}</div>
+          </MuiThemeProvider>
+
+          {selectTextField(
+            "poemType",
+            "Type of your poem",
+            poemType,
+            handlePoemTypeChange,
+            poemTypes
+          )}
+          {basicTextField("notes", "Notes of your poem", 2)}
+
+          {submitBtn}
+        </div>
+      )
     },
     {
       value: "quotes",
       label: "remember a quote",
-      formInput: <div>{submitBtn}</div>
+      formInput: <div>
+        {basicTextField("text", "A quote to remember", 3)}
+        {basicTextField("author", "Who said it?")}
+        {submitBtn}
+      </div>
     },
     {
       value: "prose",
