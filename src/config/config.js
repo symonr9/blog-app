31dfae4ae25e6914e30bca937989c689e2daadde
Symon <ramossy@oregonstate.edu>
2020-03-29 11:03:06 -0700

    Add production URL

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,14 @@
 
+
+const isDevelopmentServer = true;
+
+const PROD_URL = "https://blog-app-backend-sy.herokuapp.com/";
+const DEVL_URL = "http://localhost:2020/"
+
 export const getServerURL = (param) => {
-    return "http://localhost:2020/" + param.toString();
+    let URL = PROD_URL;
+    if(isDevelopmentServer){
+        URL = DEVL_URL;
+    }
+    return  + param.toString();
 };
