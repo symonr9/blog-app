d51defed93eb47fc992235dbeb38dba8c7ec4b8f
Symon <ramossy@oregonstate.edu>
2020-04-08 00:26:59 -0700

    Changing background color

--- src/assets/common.js
+++ src/assets/common.js
@@ -4,7 +4,34 @@ import { makeStyles } from "@material-ui/core/styles";
 //export const colors = ["white", "#FC354C", "#13747D", "#0ABFBC", "#FCF7C5"];
 
 //onaso
-export const colors = ["white", "#B3EFBB", "#5FD18C", "#AB9CD2", "#FAF692"];
+//export const colors = ["white", "#B3EFBB", "#5FD18C", "#AB9CD2", "#FAF692"];
+
+
+//grape and vanilla
+//export const colors = ["white", "#6e529b", "#5d3f6a", "#ffff99"];
+
+//blue and vanilla
+export const colors = ["white", "#8eb5eb", "#1d56a5", "#ffff99"];
+
+//blue, yellow, vanilla
+//export const colors = ["white", "#246bce", "#ffff31", "#ffff99"];
+
+//blueeee
+//export const colors = ["white", "#246bce", "#ffffff", "#a6e7ff"];
+
+//milky chocolate
+//export const colors = ["white", "#7ba9c3", "#84563c", "#fdfff5"];
+
+//pink, black, mint
+//export const colors = ["white", "#de5d83", "#010b13", "#f5fffa"];
+
+//penguin, white, blue, black
+//export const colors = ["white", "#fdfff5", "#318ce7", "#080808"];
+
+
+//export const colors = ["white", "#4e8be0", "#246bce", "#ffff99"];
+
+
 
 export const useCommonStyles = makeStyles({
   bodyDiv: {
